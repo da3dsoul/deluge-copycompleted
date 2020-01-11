90577d7a337333b44264498a29032deaad3b2911
mikepauer <illustriousdreamer@gmail.com>
2020-01-11 09:20:47 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 GazpachoKing <chase.sterling@gmail.com>
+# Copyright (C) 2011 Pedro Algarvio <pedro@algarvio.me>
+#
+# Basic plugin template created by:
+# Copyright (C) 2008 Martijn Voncken <mvoncken@gmail.com>
+# Copyright (C) 2007-2009 Andrew Resch <andrewresch@gmail.com>
+# Copyright (C) 2009 Damien Churchill <damoxc@gmail.com>
+#
+# This file is part of Deluge and is licensed under GNU General Public License 3.0, or later, with
+# the additional special exception to link portions of this program with the OpenSSL library.
+# See LICENSE for more details.
+#
+
+from setuptools import find_packages, setup
+
+__plugin_name__ = 'CopyCompleted'
+__author__ = 'Mike Pauer, Sam Lai'
+__author_email__ = 'illustriousdreamer@gmail.com, sam@edgylogic.com'
+__version__ = '2.0'
+__url__ = ''
+__license__ = 'GPLv3'
+__description__ = 'Copies finished torrents to another directory'
+__long_description__ = """"""
+__pkg_data__ = {'deluge_' + __plugin_name__.lower(): ['data/*', 'data/*/*']}
+
+setup(
+    name=__plugin_name__,
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    license=__license__,
+    long_description=__long_description__ if __long_description__ else __description__,
+    packages=find_packages(),
+    package_data=__pkg_data__,
+    entry_points="""
+    [deluge.plugin.core]
+    %s = deluge_%s:CorePlugin
+    [deluge.plugin.gtk3ui]
+    %s = deluge_%s:Gtk3UIPlugin
+    [deluge.plugin.web]
+    %s = deluge_%s:WebUIPlugin
+    """
+    % ((__plugin_name__, __plugin_name__.lower()) * 3),
+)
